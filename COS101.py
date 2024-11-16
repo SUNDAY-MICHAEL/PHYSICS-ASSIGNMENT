9f8d423bfec9a5b7c02d8d368425c538715b5853
SUNDAY-MICHAEL <ekenemichael6283@gmail.com>
2024-11-16 10:27:23 +0100

    Add files via upload

---
+++ COS101.py
@@ -0,0 +1,20 @@
+def a(mass,acceleration):
+    force=mass*acceleration
+    print (f"force={force}N")
+a(10, 30)
+def b(distance,time):
+    speed=distance/time
+    print (f"speed={speed}m/s")
+b(50,30)
+def c(force,area):
+    tensilestress=force/area
+    print (f"tensilestress={tensilestress}N/M^2")
+c(20,10)
+def d(force,distance):
+    work=force*distance
+    print(f"work={work}J")
+d(10, 10)
+def e(displacement,time):
+    velocity=displacement/time
+    print(f"velocity={velocity}m/s")
+e(50,20)
